38e3238b5ed7c9bf2ae35ac50428958349999398
d1gitale <nkalinin2020@gmail.com>
2025-03-23 11:16:38 +0300

    Realize constructors and add methods of SetCollection

---
+++ src/SetCollection.java
@@ -0,0 +1,60 @@
+public class SetCollection {
+    private int size;
+    private DynamicArray data;
+
+    public SetCollection() {
+        this.size = 0;
+        this.data = new DynamicArray();
+    }
+
+    public SetCollection(int[] values) {
+        for (int el : values) {
+            addElement(el);
+        }
+    }
+
+    public SetCollection(SetCollection set) {
+        this.size = set.getSize();
+        this.data = new DynamicArray(set.getData());
+    }
+
+    public void addElement(int element) {
+        return;
+    }
+
+    public void removeElement(int element) {
+        return;
+    }
+
+    public boolean contains(int element) {
+        return false;
+    }
+
+    public int findMax() {
+        return 0;
+    }
+
+    public int findMin() {
+        return 0;
+    }
+
+    public void iterate() {
+        return;
+    }
+
+    public SetCollection combine(SetCollection set) {
+        return new SetCollection();
+    }
+
+    public boolean areEqual(SetCollection set) {
+        return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public DynamicArray getData() {
+        return data;
+    }
+}
